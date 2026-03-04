5ed3baafd3f3c1e2ac04c2108c5577be3f47d44a
Yakamozm <joshuafollente@gmail.com>
2026-03-04 14:44:36 -0800

    Updated script.js
    Added back button function that is incomplete and added debug features for future developers.

--- src/script.js
+++ src/script.js
@@ -2,7 +2,9 @@
 const page_view = /** @type {HTMLDivElement} */ (document.querySelector('.current-page'));
 const visible_page_counters = /** @type {NodeListOf<HTMLSpanElement>} */ (document.querySelectorAll('[class^=visible-page]'));
 let current_page_number = 0;
-const [...templates] = /** @type {NodeListOf<HTMLTemplateElement>} */ (document.querySelectorAll('template'));
+const templates = [...document.querySelectorAll('template')];
+const nextBtn = (document.querySelector('.next'));
+const backBtn = (document.querySelector('.back'));
 
 function next_page() {
     const fragment = templates[current_page_number++].content.cloneNode(true);
@@ -11,6 +13,23 @@ function next_page() {
     for (const counter of visible_page_counters) {
         counter.textContent = `${number++}`;
     }
+    console.log(templates);
+    console.log(current_page_number);
+}
+
+function back_page() {
+    const fragment = templates[current_page_number--].content.cloneNode(true);
+    page_view.replaceChildren(fragment);
+    let number = current_page_number;
+    for (const counter of visible_page_counters) {
+        counter.textContent = `${number++}`;
+    }
+    console.log(templates);
+    console.log(current_page_number);
 }
 
 next_page();
+
+nextBtn.addEventListener('click', next_page);
+backBtn.addEventListener('click', back_page);
+
