f8076f6258d75577e23693c971d65f82fabb9324
CARTeeyuh <ethanpua10@gmail.com>
2026-03-09 13:35:37 -0700

    did css, js, fixed some html

--- script.js
+++ script.js
@@ -6,30 +6,73 @@ const templates = [...document.querySelectorAll('template')];
 const nextBtn = (document.querySelector('.next'));
 const backBtn = (document.querySelector('.back'));
 
-function next_page() {
-    const fragment = templates[current_page_number++].content.cloneNode(true);
+function render_page() {
+    const fragment = templates[current_page_number].content.cloneNode(true);
     page_view.replaceChildren(fragment);
-    let number = current_page_number;
+
+    let number = current_page_number + 1;
     for (const counter of visible_page_counters) {
         counter.textContent = `${number++}`;
     }
-    console.log(templates);
-    console.log(current_page_number);
+}
+
+function next_page() {
+    if (current_page_number >= templates.length - 1) return;
+
+    current_page_number++;
+    render_page();
 }
 
 function back_page() {
-    const fragment = templates[current_page_number--].content.cloneNode(true);
-    page_view.replaceChildren(fragment);
-    let number = current_page_number;
-    for (const counter of visible_page_counters) {
-        counter.textContent = `${number++}`;
-    }
-    console.log(templates);
-    console.log(current_page_number);
+    if (current_page_number <= 0) return;
+
+    current_page_number--;
+    render_page();
 }
 
-next_page();
+render_page();
 
 nextBtn.addEventListener('click', next_page);
 backBtn.addEventListener('click', back_page);
 
+import { fetchJson } from './utility.js';
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+
+async function init() {
+    const root = document.querySelector('#root');
+    const url = 'https://eecu-data-server.vercel.app/data';
+
+    try {
+        const jobs = await fetchJson(url);
+        root.append(buildList(jobs));
+    } catch (err) {
+        root.style.color = 'red';
+        root.textContent = `Error: ${err.message}`;
+    }
+}
+
+function buildList(jobs = []) {
+    const frag = document.createDocumentFragment();
+    for (const { Occupation, Salary } of jobs) {
+      const section = document.createElement('section');
+  
+      const occ = document.createElement('div');
+      occ.innerHTML = `<strong>Occupation</strong>: ${Occupation}`;
+      section.append(occ);
+  
+      const sal = document.createElement('div');
+      sal.innerHTML = `<strong>Salary</strong>: ${formatter.format(Salary)}`;
+      section.append(sal);
+  
+      frag.append(section);
+    }
+    return frag;
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
+
+  const careerList = document.getElementById
